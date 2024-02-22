50d1a2f2c4e94348b185c2066c51d85cd013f688
Shivesh45 <shiveshravimishra@gmail.com>
2024-02-22 12:01:18 +0545

    change gareko

--- file_handiling.py
+++ file_handiling.py
@@ -1,5 +1,19 @@
-f=open('hello.txt','a')
-# print(f.readline()) 
-# for x in f:
-#    print(f.readline(4))
-f.write('k xa keta ho\n') 
+# f=open('hello.txt','w')
+# # print(f.readline()) 
+# # for x in f:
+# #    print(f.readline(4))
+# f.write('k xa keta ho') 
+
+def create_file():
+   
+    filename = input("Enter the filename: ")
+    content = input("Enter the content: ")
+    try:
+        with open(filename, 'w') as file:
+            file.write(content)
+        print("File '{filename}' created successfully with the provided content.")
+    except Exception as e:
+        print("Error occurred while creating the file: {e}")
+
+if __name__ == "__main__":
+    create_file()
